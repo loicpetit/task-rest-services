bd460960670987ada771881726781b8e53282767
Loic PETIT <neo_cloud@hotmail.com>
2018-08-27 15:50:40 +0200

    Add log management.

--- app/docker/docker-create.ps1
+++ app/docker/docker-create.ps1
@@ -1,1 +1,1 @@
-docker create --name task-rest-services -p 8080:8080 -v $pwd\..\target\task-rest-services-1.0.0.jar:/app/task-rest-services.jar portepoisse/task-rest-services:latest
+docker create --name task-rest-services -p 8080:8080 -v $pwd\..\target\task-rest-services-1.0.0.jar:/app/task-rest-services.jar -v $pwd\..\logs\:/app/logs/ -v $pwd\..\properties\:/app/config/ portepoisse/task-rest-services:latest
--- app/src/main/java/com/capgemini/tasks/TaskController.java
+++ app/src/main/java/com/capgemini/tasks/TaskController.java
@@ -2,6 +2,9 @@ package com.capgemini.tasks;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     private final TaskCollection tasks = new TaskCollection();
 
@@ -27,25 +32,30 @@ public class TaskController {
 
     @RequestMapping("/tasks")
     public TaskCollection getTasks() {
+        logger.info("Get tasks...");
         return tasks;
     }
 
     @RequestMapping("tasks/new")
     public Task createTask(@RequestParam("message") String message){
+        logger.info("Add task...");
         if(tasks.add(message)){
             return tasks.get(tasks.size()-1);
         }
+        logger.error("Add task failed !");
         throw new IllegalArgumentException("L'ajout de la tâche a échoué");
     }
 
     @RequestMapping("/tasks/{taskId}")
     public Task getTask(@PathVariable("taskId") long taskId){
+        logger.info("Get task {}...", taskId);
         int index = getIndex(taskId);
         return tasks.get(index);
     }
 
     @RequestMapping("tasks/{taskId}/remove")
     public void removeTask(@PathVariable("taskId") long taskId){
+        logger.info("Remove task {}...", taskId);
         int index = getIndex(taskId);
         tasks.remove(index);
     }
@@ -53,6 +63,7 @@ public class TaskController {
     private int getIndex(long taskId){
         int index = tasks.indexOf(taskId);
         if(index < 0){
+            logger.error("L'id {} n'existe pas", taskId);
             throw new IllegalArgumentException("L'id n'existe pas");
         }
         return index;
