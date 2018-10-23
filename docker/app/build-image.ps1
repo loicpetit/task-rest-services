e95f3034dac62dfccc21d10f02385f70e267e4d0
Loic PETIT <loic.petit@capgemini.com>
2018-10-23 18:49:39 +0200

    add labels

--- docker/app/build-image.ps1
+++ docker/app/build-image.ps1
@@ -1,3 +1,5 @@
 $path = "$pwd\..\..\app"
 mvn clean package -f $path
-docker build -t portepoisse/task-rest-services:latest $path
+$buildDate = (Get-Date).ToString("yyyy-MM-ddThh:mm:ssZ")
+$version = "1.0.0"
+docker build --build-arg BUILD_DATE=$buildDate --build-arg BUILD_VERSION=$version -t portepoisse/task-rest-services:latest $path
